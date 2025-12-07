10eea0085ca5fe5fc83b857b9a0d390f84da0600
areebagillanii <areebagillani938@gmail.com>
2025-12-07 18:30:06 +0500

    added lab tasks

--- pf lab 1/pa lab 1 task 4.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	cout<<"she said ,lets's learn c++\"\n";
-	cout<<"to start, save your file in c:\\c++_projects\\lab1\n";
-	cout<<"good luck\n";
-	return 0;
-}
